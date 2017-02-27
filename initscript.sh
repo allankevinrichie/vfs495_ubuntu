0ea5cbc9f11c1b875a71098584b5f2e1c6495263
Petre Boboc <bobocpetre98@gmail.com>
2017-02-27 21:30:02 +0200

    initscript.sh now downloads required files

--- initscript.sh
+++ initscript.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+wget https://raw.githubusercontent.com/PetreBoboc/vfs495_ubuntu/master/vcsFPServiceDaemon
 sudo cp vcsFPServiceDaemon /etc/init.d/
 sudo update-rc.d vcsFPServiceDaemon defaults
 sudo /etc/init.d/vcsFPServiceDaemon start
